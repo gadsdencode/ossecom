c103b1b7b48f637c034c7544bb119f978d6e9f0a
gadsdencode <jordan.martens@osscontact.com>
2023-10-05 08:18:04 -0400

    REDIS environment variables added and config updated

--- medusa-config.js
+++ medusa-config.js
@@ -55,18 +55,19 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
-      redisUrl: REDIS_URL
+      redisUrl: process.env.EVENTS_REDIS_URL
     }
   },
   cacheService: {
     resolve: "@medusajs/cache-redis",
     options: {
-      redisUrl: REDIS_URL
+      redisUrl: process.env.CACHE_REDIS_URL,
+      ttl: 30,
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
